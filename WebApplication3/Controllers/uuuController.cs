36f2e11039e60e98909a29717c05b12def52d6e9
周嘉辉 <@>
2020-04-02 14:09:16 +0800

    2

---
+++ WebApplication3/Controllers/uuuController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class uuuController : Controller
+    {
+        // GET: uuu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Call()
+        {
+            return View();
+        }
+        public ActionResult AAA()
+        {
+            return View();
+        }
+    }
+}
